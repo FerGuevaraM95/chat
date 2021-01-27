a96879d03888fa2aa82a337e7faa1cd3390f4261
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-01-26 23:59:24 -0600

    (Add): Auth routes

--- models/server.js
+++ models/server.js
@@ -27,6 +27,11 @@ class Server {
   middlewares() {
     // Desplegar el directorio público
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    // Cors
+
+    // API endpoints
+    this.app.use('/api/login', require('../router/auth'));
   }
 
   // Esta configuración se puede tener aquí o como propieda de clase
