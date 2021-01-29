2b4636e7689b65abb12e693f0862b0c4e2cec8b4
Fernando Guevara <luisfernandoguevara95@gmail.com>
2021-01-28 21:14:36 -0600

    (Add): Create user in DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,38 @@
+const User = require('../models/user');
 
 const createUser = async (req, res) => {
-  res.json({
-    ok: true,
-    msg: 'new'
-  });
+  
+
+  try {
+
+    const {email, password} = req.body;
+
+    const existEmail = await User.findOne({email});
+
+    if(existEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya existe'
+      });
+    };
+
+    // Guardar usuario en DB
+    const user = new User(req.body);
+    await user.save();
+
+    res.json({
+      user
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+    
+  };
+
 };
 
 const login = async (req, res) => {
